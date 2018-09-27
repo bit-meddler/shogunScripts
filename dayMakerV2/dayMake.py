29a602b856cb559504bda6c329b4d17ba9cc15dd
RichH <RichH@beacon-optical.co.uk>
2018-09-27 17:06:29 +0100

    descoped anon widgets, implemented UI Locks, stubbed functions

--- dayMakerV2/dayMake.py
+++ dayMakerV2/dayMake.py
@@ -11,15 +11,24 @@ class DayBuild( QtGui.QWidget ):
         self._buildUI()
         
         
-    def _loadLastSettings( self ):
-        # TODO: Load from ini file
-        self.current_client  = "Framestore"
-        self.current_project = "Gravity"
+    def _updateCpMap( self ):
+        # Scan Folder structure?
         self.cp_map = {
             "Framestore" : ( "Gravity", "Jungle Book" ),
             "Weta" : ( "Apes", "Tintin", "LotR" ),
             "dNeg" : ( "John Carter", "Avengers" ),
         }
+    
+    
+    def _loadLastSettings( self ):
+        # TODO: Load from ini file
+        self.current_client   = "Framestore"
+        self.current_project  = "Gravity"
+        self.current_location = "A"
+        self.current_stage    = 1
+        # update map
+        self._updateCpMap()
+        # set UI
         self.client_list  = sorted( self.cp_map.keys() )
         self.client_idx   = self.client_list.index( self.current_client )
         self.project_list = sorted( self.cp_map[ self.current_client ] )
@@ -27,103 +36,120 @@ class DayBuild( QtGui.QWidget ):
 
         
     def _updateCpUi( self ):
-        self.clients.clear() 
-        self.projects.clear()
-        self.clients.addItems( self.client_list )
-        self.clients.setCurrentIndex( self.client_idx )
-        self.projects.addItems( self.project_list )
-        self.projects.setCurrentIndex( self.project_idx )
-
+        # update combos
+        self._client_combo.clear() 
+        self._project_combo.clear()
+        self._client_combo.addItems( self.client_list )
+        self._client_combo.setCurrentIndex( self.client_idx )
+        self._project_combo.addItems( self.project_list )
+        self._project_combo.setCurrentIndex( self.project_idx )
+        # Look at client/project current stage?
         
+    def _projectLockCB( self ):
+        lock = not self._project_lock.isChecked()
+        self._new_prj.setEnabled( lock )
+        self._client_combo.setEnabled( lock )
+        self._project_combo.setEnabled( lock )
+        
+        
+    def _dateLockCB( self ):
+        lock = not self._date_lock.isChecked()
+        self._date_code.setReadOnly( lock )
+    
+    
     def _buildUI( self ):
         self.setWindowTitle( "Make my Day - V2.0.1" )
-        self.boxWidth = 100
+        boxWidth = 100
         
-        # Layout gropup
-        self.vbox = QtGui.QVBoxLayout()
-
+        anon = None
+        
         # Select Project Section #################################################
-        self.sp_grp = QtGui.QGroupBox( "Select Project" )
-        self.sp_grid = QtGui.QGridLayout()
+        tmp_sp_grp  = QtGui.QGroupBox( "Select Project" )
+        tmp_sp_grid = QtGui.QGridLayout()
         
-        self.sp_cli_lbl = QtGui.QLabel( "Client", self )
-        self.sp_grid.addWidget( self.sp_cli_lbl, 0, 0, 1, 2 )
+        anon = QtGui.QLabel( "Client", self )
+        tmp_sp_grid.addWidget( anon, 0, 0, 1, 2 )
         
-        self.clients = QtGui.QComboBox( self )
-        self.clients.setMinimumWidth( self.boxWidth )
-        self.sp_grid.addWidget( self.clients, 0, 2, 1, 3 )
+        self._client_combo = QtGui.QComboBox( self )
+        self._client_combo.setMinimumWidth( boxWidth )
+        tmp_sp_grid.addWidget( self._client_combo, 0, 2, 1, 3 )
         
-        self.prj_lbl = QtGui.QLabel( "Project", self )
-        self.sp_grid.addWidget( self.prj_lbl, 1, 0, 1, 2 )
+        self._new_prj = QtGui.QPushButton( "New Project", self )
+        tmp_sp_grid.addWidget( self._new_prj, 0, 5, 1, 2 )
+        
+        anon = QtGui.QLabel( "Project", self )
+        tmp_sp_grid.addWidget( anon, 1, 0, 1, 2 )
 
-        self.projects = QtGui.QComboBox( self )
-        self.projects.setMinimumWidth( self.boxWidth )
-        self.sp_grid.addWidget( self.projects, 1, 2, 1, 3 )
+        self._project_combo = QtGui.QComboBox( self )
+        self._project_combo.setMinimumWidth( boxWidth )
+        tmp_sp_grid.addWidget( self._project_combo, 1, 2, 1, 3 )
         
-        self.prj_lock = QtGui.QCheckBox( "Locked", self )
-        self.prj_lock.setCheckState( QtCore.Qt.Checked )
-        self.sp_grid.addWidget( self.prj_lock, 1, 6 )
+        self._project_lock = QtGui.QCheckBox( "Locked", self )
+        self._project_lock.setCheckState( QtCore.Qt.Checked )
+        self._project_lock.stateChanged.connect( self._projectLockCB )
+        tmp_sp_grid.addWidget( self._project_lock, 1, 6 )
         
-        self.prj_path = QtGui.QLineEdit( "Path", self )
-        self.prj_path.setReadOnly( True )
-        self.sp_grid.addWidget( self.prj_path, 2, 0, 1, 7 )
+        self._project_path = QtGui.QLineEdit( "Path", self )
+        self._project_path.setReadOnly( True )
+        tmp_sp_grid.addWidget( self._project_path, 2, 0, 1, 7 )
         
-        self.prj_pick_bt = QtGui.QPushButton( "I Chose YOU!", self )
-        self.sp_grid.addWidget( self.prj_pick_bt, 3, 0, 1, 3 )
+        anon = QtGui.QPushButton( "I Chose YOU!", self )
+        tmp_sp_grid.addWidget( anon, 3, 0, 1, 3 )
         
-        self.prj_update_bt = QtGui.QPushButton( "Update Henchman", self )
-        self.sp_grid.addWidget( self.prj_update_bt, 3, 4, 1, 3 )
+        anon = QtGui.QPushButton( "Update Henchman", self )
+        tmp_sp_grid.addWidget( anon, 3, 4, 1, 3 )
         
-        self.sp_grp.setLayout( self.sp_grid )
+        tmp_sp_grp.setLayout( tmp_sp_grid )
 
         # Create day #############################################################
-        self.cd_grp = QtGui.QGroupBox( "Create Day" )
-        self.cd_grid = QtGui.QGridLayout()
+        tmp_cd_grp  = QtGui.QGroupBox( "Create Day" )
+        tmp_cd_grid = QtGui.QGridLayout()
         
-        self.cd_date_lbl = QtGui.QLabel( "Datecode", self )
-        self.cd_grid.addWidget( self.cd_date_lbl, 0, 0, 1, 1 )
+        anon = QtGui.QLabel( "Datecode", self )
+        tmp_cd_grid.addWidget( anon, 0, 0, 1, 1 )
         
-        self.cd_date = QtGui.QLineEdit( "123456", self )
-        self.cd_date.setMaximumWidth( 75 )
-        self.cd_date.setReadOnly( True )
-        self.cd_grid.addWidget( self.cd_date, 1, 0, 1, 1 )
+        self._date_code = QtGui.QLineEdit( "123456", self )
+        self._date_code.setMaximumWidth( 75 )
+        tmp_cd_grid.addWidget( self._date_code, 1, 0, 1, 1 )
         
-        self.cd_stageId_lbl = QtGui.QLabel( "Stage ID:", self )
-        self.cd_grid.addWidget( self.cd_stageId_lbl, 1, 1, 1, 1 )
+        anon = QtGui.QLabel( "Stage ID:", self )
+        tmp_cd_grid.addWidget( anon, 1, 1, 1, 1 )
         
-        self.cd_loc_lbl = QtGui.QLabel( "Location", self )
-        self.cd_grid.addWidget( self.cd_loc_lbl, 0, 2, 1, 1 )
+        anon = QtGui.QLabel( "Location", self )
+        tmp_cd_grid.addWidget( anon, 0, 2, 1, 1 )
         
-        self.cd_stage_lbl = QtGui.QLabel( "Stage", self )
-        self.cd_grid.addWidget( self.cd_stage_lbl, 0, 3, 1, 1 )
+        anon = QtGui.QLabel( "Stage", self )
+        tmp_cd_grid.addWidget( anon, 0, 3, 1, 1 )
         
-        self.cd_loc = QtGui.QComboBox( self )
-        self.cd_loc.addItems( list( "ABCDEFGH" ) ) 
-        self.cd_grid.addWidget( self.cd_loc, 1, 2, 1, 1 )
+        self._location = QtGui.QComboBox( self )
+        self._location.addItems( list( "ABCDEFGH" ) ) 
+        tmp_cd_grid.addWidget( self._location, 1, 2, 1, 1 )
         
-        self.cd_stg = QtGui.QSpinBox( self )
-        self.cd_stg.setMinimum(  1 )
-        self.cd_stg.setMaximum( 99 )
-        self.cd_grid.addWidget( self.cd_stg, 1, 3, 1, 1 )
+        self._stage = QtGui.QSpinBox( self )
+        self._stage.setRange( 1, 99 )
+        tmp_cd_grid.addWidget( self._stage, 1, 3, 1, 1 )
         
-        self.date_lock = QtGui.QCheckBox( "Unlock Datecode", self )
-        self.date_lock.setCheckState( QtCore.Qt.Unchecked )
-        self.cd_grid.addWidget( self.date_lock, 2, 0, 1, 2 )
+        self._date_lock = QtGui.QCheckBox( "Unlock Datecode", self )
+        self._date_lock.setCheckState( QtCore.Qt.Unchecked )
+        self._date_lock.stateChanged.connect( self._dateLockCB )
+        tmp_cd_grid.addWidget( self._date_lock, 2, 0, 1, 2 )
         
-        self.gen_bt = QtGui.QPushButton( "Generate", self )
-        self.cd_grid.addWidget( self.gen_bt, 2, 3, 1, 1 )
+        anon = QtGui.QPushButton( "Generate", self )
+        tmp_cd_grid.addWidget( anon, 2, 3, 1, 1 )
         
-        self.cd_grp.setLayout( self.cd_grid )
+        tmp_cd_grp.setLayout( tmp_cd_grid )
 
         # Assemble UI ###############################################################
-        self.vbox.addWidget( self.sp_grp )
-        self.vbox.addWidget( self.cd_grp )
-        self.vbox.addStretch( 1 )
-        self.setLayout( self.vbox ) 
+        tmp_vbox = QtGui.QVBoxLayout()
+        tmp_vbox.addWidget( tmp_sp_grp )
+        tmp_vbox.addWidget( tmp_cd_grp )
+        tmp_vbox.addStretch( 1 )
+        self.setLayout( tmp_vbox ) 
         
         # generate comboBox items
         self._updateCpUi()
-        
+        self._projectLockCB()
+        self._dateLockCB()
 
         
     def run( self ):
