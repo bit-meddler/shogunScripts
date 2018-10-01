edb2754b57ed6245a6f293e75cdab9aca5d77302
RichH <RichH@beacon-optical.co.uk>
2018-10-01 12:27:25 +0100

    getting close to good UI wireframe, will soon need guts.

--- dayMakerV2/dayMake.py
+++ dayMakerV2/dayMake.py
@@ -1,5 +1,5 @@
 from PySide import QtGui, QtCore
-import sys
+import sys, os
 
 
 class DayBuild( QtGui.QWidget ):
@@ -13,6 +13,7 @@ class DayBuild( QtGui.QWidget ):
         
     def _updateCpMap( self ):
         # Scan Folder structure?
+        print( "Scan session" )
         self.cp_map = {
             "Framestore" : ( "Gravity", "Jungle Book" ),
             "Weta" : ( "Apes", "Tintin", "LotR" ),
@@ -20,12 +21,20 @@ class DayBuild( QtGui.QWidget ):
         }
     
     
+    def _getProjectSettings( self ):
+        print( "Get Project Settings" )
+        self._sessions        = ( "AM", "PM", "#CAL#", "#ROM#" )
+        self.current_location = "B"
+        self.current_stage    = 2
+        
+    
     def _loadLastSettings( self ):
         # TODO: Load from ini file
+        self.vicon_root       = "C:\\ViconDB\\"
+        self.day_format       = "{daycode}_{location}{stage}_{dayname}"
         self.current_client   = "Framestore"
         self.current_project  = "Gravity"
-        self.current_location = "A"
-        self.current_stage    = 1
+        #self._getProjectSettings()
         # update map
         self._updateCpMap()
         # set UI
@@ -35,7 +44,20 @@ class DayBuild( QtGui.QWidget ):
         self.project_idx  = self.project_list.index( self.current_project )
 
         
+    def _updatePath( self ):
+        self.project_idx = self._project_combo.currentIndex()
+        self.current_project = self._project_combo.itemText( self.project_idx )
+        # Compose 
+        self.prj_path = "{}{}{}".format( self.current_client, os.path.sep, self.current_project )
+        self._project_path.setText( self.prj_path )
+        # Get Project Settings
+        self._getProjectSettings()
+    
+    
     def _updateCpUi( self ):
+        # Sanity test
+        if( ( len( self.client_list ) == 0 ) or ( len( self.project_list ) == 0 ) ):
+            return
         # update combos
         self._clients_combo.clear() 
         self._project_combo.clear()
@@ -51,11 +73,18 @@ class DayBuild( QtGui.QWidget ):
         self._new_prj.setEnabled( lock )
         self._clients_combo.setEnabled( lock )
         self._project_combo.setEnabled( lock )
+        if not lock: # When applying lock, update
+            self._updatePath()
         
         
     def _dateLockCB( self ):
         lock = not self._date_lock.isChecked()
         self._date_code.setReadOnly( lock )
+        
+        
+    def _chooseCP( self, innerCall=False ):
+        # get selected project
+        self._project_lock.setCheckState( QtCore.Qt.Checked )
     
     
     def _clientChangeCB( self ):
@@ -74,6 +103,30 @@ class DayBuild( QtGui.QWidget ):
         # Hack to dodge Recursion error
         self._clients_combo.currentIndexChanged.connect( self._clientChangeCB )
         
+        
+    def _setStage( self ):
+        loc_idx = self._location.findText( self.current_location )
+        self._location.setCurrentIndex( loc_idx )
+        self._stage.setValue( self.current_stage )
+        
+        
+    def _stageCB( self ):
+        self.current_location = self._location.itemText( self._location.currentIndex() )
+        self.current_stage = self._stage.value()
+        
+        
+    def generate( self ):
+        prj_path = self.vicon_root + self.prj_path + os.sep
+        meta_data = {
+            "daycode" : self._date_code.text(),
+            "location": self.current_location,
+            "stage"   : self.current_stage,
+            "dayname" : self._session_name.text()
+        }
+        day_code = self.day_format.format( **meta_data )
+        print( "CreateDay -Prj '{}' -Day '{}'".format( prj_path, day_code ) )
+        for session in self._sessions:
+            print( "CreateSession -Prj '{}' -Day '{}' -Session '{}'".format( prj_path, day_code, session ) )
         
     def _buildUI( self ):
         self.setWindowTitle( "Make my Day - V2.0.1" )
@@ -110,9 +163,10 @@ class DayBuild( QtGui.QWidget ):
         self._project_path.setReadOnly( True )
         tmp_sp_grid.addWidget( self._project_path, 2, 0, 1, 7 )
         
-        anon = QtGui.QPushButton( "I &Chose YOU!", self )
-        anon.setMinimumWidth( 110 )
-        tmp_sp_grid.addWidget( anon, 3, 0, 1, 3 )
+        self._choose = QtGui.QPushButton( "I &Chose YOU!", self )
+        self._choose.setMinimumWidth( 110 )
+        self._choose.setAutoRepeat( False )
+        tmp_sp_grid.addWidget( self._choose, 3, 0, 1, 3 )
         
         anon = QtGui.QPushButton( "&Update Henchman", self )
         tmp_sp_grid.addWidget( anon, 3, 4, 1, 3 )
@@ -131,6 +185,7 @@ class DayBuild( QtGui.QWidget ):
         tmp_cd_grid.addWidget( self._date_code, 1, 0, 1, 1 )
         
         anon = QtGui.QLabel( "Stage ID:", self )
+        anon.setAlignment( QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter )
         tmp_cd_grid.addWidget( anon, 1, 1, 1, 1 )
         
         anon = QtGui.QLabel( "Location", self )
@@ -147,12 +202,20 @@ class DayBuild( QtGui.QWidget ):
         self._stage.setRange( 1, 99 )
         tmp_cd_grid.addWidget( self._stage, 1, 3, 1, 1 )
         
+        anon = QtGui.QLabel( "Description", self )
+        anon.setAlignment( QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter )
+        tmp_cd_grid.addWidget( anon, 2, 0, 1, 1 )
+        
+        self._session_name = QtGui.QLineEdit( "CaptureDay", self )
+        self._session_name.setReadOnly( False )
+        tmp_cd_grid.addWidget( self._session_name, 2, 1, 1, 4 )
+        
         self._date_lock = QtGui.QCheckBox( "Unlock Datecode", self )
         self._date_lock.setCheckState( QtCore.Qt.Unchecked )
-        tmp_cd_grid.addWidget( self._date_lock, 2, 0, 1, 2 )
+        tmp_cd_grid.addWidget( self._date_lock, 3, 0, 1, 2 )
         
         self._generate = QtGui.QPushButton( "&Generate Day", self )
-        tmp_cd_grid.addWidget( self._generate, 2, 3, 1, 1 )
+        tmp_cd_grid.addWidget( self._generate, 3, 3, 1, 1 )
         
         tmp_cd_grp.setLayout( tmp_cd_grid )
 
@@ -166,13 +229,23 @@ class DayBuild( QtGui.QWidget ):
         # generate comboBox items
         self._updateCpUi()
         self._projectLockCB()
-        self._dateLockCB()
+        # self._dateLockCB()
+        # self._updatePath()
+        self._setStage()
+        self._stageCB()
         
         # attach CBs
+        self._choose.clicked.connect( self._chooseCP )
+        #self.connect( self._choose, SIGNAL( 'clicked()' ), self._chooseCP )
+        self._generate.clicked.connect( self.generate )
+        
         self._project_lock.stateChanged.connect( self._projectLockCB )
         self._date_lock.stateChanged.connect( self._dateLockCB )
+        
         self._clients_combo.currentIndexChanged.connect( self._clientChangeCB )
-
+        self._location.currentIndexChanged.connect( self._stageCB )
+        self._stage.valueChanged.connect( self._stageCB )
+        
         
     def run( self ):
         self.show()
